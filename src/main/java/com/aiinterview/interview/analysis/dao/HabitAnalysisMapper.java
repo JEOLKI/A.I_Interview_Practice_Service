3473e7a263db7f7ce6108ecc2ae37a8947e46843
ktk <ktk3981@gmail.com>
2020-12-01 16:55:49 +0900

    언어분석 패키지구성 수정

--- src/main/java/com/aiinterview/interview/analysis/dao/HabitAnalysisMapper.java
+++
@@ -1,8 +0,0 @@
-package com.aiinterview.interview.analysis.dao;
-
-import egovframework.rte.psl.dataaccess.mapper.Mapper;
-
-@Mapper("habitAnalysisMapper")
-public interface HabitAnalysisMapper {
-
-}
--- src/main/java/com/aiinterview/interview/analysis/dao/KeywordAnalysisMapper.java
+++
@@ -1,8 +0,0 @@
-package com.aiinterview.interview.analysis.dao;
-
-import egovframework.rte.psl.dataaccess.mapper.Mapper;
-
-@Mapper("keywordAnalysisMapper")
-public interface KeywordAnalysisMapper {
-
-}
