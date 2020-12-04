8cee9b80680e311e3febdf95b876b056b3bdd68e
jeonggi <hongjeonggi35@gmail.com>
2020-12-04 11:59:03 +0900

    ready jsp interviewController QuestionVO

--- src/main/java/com/aiinterview/interview/web/InterviewController.java
+++ src/main/java/com/aiinterview/interview/web/InterviewController.java
@@ -3,8 +3,11 @@ package com.aiinterview.interview.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.aiinterview.interview.vo.QuestionVO;
 
 @RequestMapping("/interview")
 @Controller
@@ -18,8 +21,9 @@ public class InterviewController {
 		return "interview/ready";
 	}
 	
-	@RequestMapping(path="/setting.do",method = { RequestMethod.GET })
-	public String interviewSetting() {
+	@RequestMapping(path="/setting.do", method = { RequestMethod.GET })
+	public String interviewSetting(QuestionVO questionList, Model model) {
+		model.addAttribute(questionList);
 		logger.debug("InterviewController.interviewSetting()진입");
 		return "interview/setting";
 	}
