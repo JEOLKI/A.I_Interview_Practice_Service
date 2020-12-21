349f967e524b668ac427b67b10fa7eee426d7941
deewaes <ismh5279@gmail.com>
2020-12-22 08:30:44 +0900

    로그인 컨트롤러 테스트 코드

--- src/test/java/com/aiinterview/member/web/LoginControllerTest.java
+++ src/test/java/com/aiinterview/member/web/LoginControllerTest.java
@@ -18,11 +18,74 @@ public class LoginControllerTest extends WebTestConfig {
 	private static final Logger logger = LoggerFactory.getLogger(LoginControllerTest.class);
 
 	@Test
-	public void viewTest() throws Exception {
+	public void loginTest() throws Exception {
 		mockMvc.perform(post("/login/process.do")
 				.param("memId", "TEST_ID")
 				.param("memPw","TEST_PW"))
 		.andExpect(status().is(302))
 		.andExpect(view().name("redirect:/login/home.do"));
+	}
+	
+	@Test
+	public void viewTest() throws Exception {
+		mockMvc.perform(get("/login/main.do"))
+		.andExpect(status().is(200))
+		.andExpect(view().name("login/main"));
+	}
+	
+	@Test
+	public void homeTest() throws Exception {
+		mockMvc.perform(get("/login/home.do"))
+		.andExpect(status().is(200))
+		.andExpect(view().name("login/home"));
+	}
+	
+	@Test
+	public void logoutTest() throws Exception {
+		mockMvc.perform(get("/login/logout.do"))
+		.andExpect(status().is(200))
+		.andExpect(view().name("login/main"));
+	}
+	
+	@Test
+	public void managerViewTest() throws Exception{
+		mockMvc.perform(get("/login/manage.do"))
+		.andExpect(status().is(200))
+		.andExpect(view().name("manage/main"));
+	}
+	
+	@Test
+	public void manageStatisticTest() throws Exception{
+		mockMvc.perform(get("/login/manageStatistic.do"))
+		.andExpect(status().is(200))
+		.andExpect(view().name("manage/manageStatistic"));
+	}
+	
+	@Test
+	public void helpTest() throws Exception{
+		mockMvc.perform(get("/login/help.do"))
+		.andExpect(status().is(200))
+		.andExpect(view().name("login/help"));
+	}
+	
+	@Test
+	public void serviceIntroTest() throws Exception{
+		mockMvc.perform(get("/login/serviceIntro.do"))
+		.andExpect(status().is(200))
+		.andExpect(view().name("login/serviceIntro"));
+	}
+	
+	@Test
+	public void userAgreeTest() throws Exception{
+		mockMvc.perform(get("/login/userAgree.do"))
+		.andExpect(status().is(200))
+		.andExpect(view().name("agreement/userAgree"));
+	}
+	
+	@Test
+	public void personalAgreeTest() throws Exception{
+		mockMvc.perform(get("/login/personalAgree.do"))
+		.andExpect(status().is(200))
+		.andExpect(view().name("agreement/personalAgree"));
 	}
 }
