a0d185f37023437f6453996bdc32ffc825a7d5bb
deewaes <ismh5279@gmail.com>
2020-12-21 14:51:41 +0900

    음성분석 테스트코드(맵퍼, 서비스)

---
+++ src/test/java/com/aiinterview/interview/dao/VoiceAnalysisMapperTest.java
@@ -0,0 +1,60 @@
+package com.aiinterview.interview.dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+
+import com.aiinterview.ModelTestConfig;
+import com.aiinterview.analysis.dao.VoiceAnalysisMapper;
+import com.aiinterview.analysis.vo.VoiceAnalysisVO;
+
+public class VoiceAnalysisMapperTest extends ModelTestConfig{
+	
+	@Resource(name="voiceAnalysisMapper")
+	private VoiceAnalysisMapper voiceAnalysisMapper;
+
+	@Test
+	public void retrieveListTest() throws Exception {
+		/***Given***/
+		
+
+		/***When***/
+		List<VoiceAnalysisVO> voiceAnalysisList = voiceAnalysisMapper.retrieveList("1");
+
+		/***Then***/
+		assertEquals(50, voiceAnalysisList.size());
+	}
+	
+	@Test // voiceAnalysisSq가 위배된다 추후  디비 교체하면 문제없음
+	public void createTest() throws Exception {
+		/***Given***/
+		VoiceAnalysisVO voiceAnalysisVO = new VoiceAnalysisVO();
+		voiceAnalysisVO.setAnsSq("5");
+		voiceAnalysisVO.setVoiceDecibel("80");
+		voiceAnalysisVO.setVoiceRange("60");
+		
+		/***When***/
+		voiceAnalysisMapper.create(voiceAnalysisVO);
+		
+		/***Then***/
+		assertTrue(voiceAnalysisVO.getVoiceAnalysisSq() !="0");
+	}
+	
+	@Test
+	public void retrieveGrowthTest() throws Exception {
+		/***Given***/
+
+		
+		/***When***/
+		List<VoiceAnalysisVO> voiceAnalysisList = voiceAnalysisMapper.retrieveGrowth("MEMBER1");
+		
+		/***Then***/
+		assertEquals(5, voiceAnalysisList.size());
+	}
+
+}
---
+++ src/test/java/com/aiinterview/interview/service/VoiceAnalysisServiceTest.java
@@ -0,0 +1,44 @@
+package com.aiinterview.interview.service;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+
+import com.aiinterview.ModelTestConfig;
+import com.aiinterview.analysis.service.VoiceAnalysisService;
+import com.aiinterview.analysis.vo.VoiceAnalysisVO;
+
+public class VoiceAnalysisServiceTest extends ModelTestConfig {
+
+	@Resource(name="voiceAnalysisService")
+	private VoiceAnalysisService voiceAnalysisService;
+	
+	@Test
+	public void retrieveListTest() throws Exception {
+		/***Given***/
+		
+
+		/***When***/
+		List<VoiceAnalysisVO> voiceAnalysisList = voiceAnalysisService.retrieveList("1");
+
+		/***Then***/
+		assertEquals(50, voiceAnalysisList.size());
+	}
+	
+	@Test
+	public void retrieveGrowthTest() throws Exception {
+		/***Given***/
+
+		
+		/***When***/
+		List<VoiceAnalysisVO> voiceAnalysisList = voiceAnalysisService.retrieveGrowth("MEMBER1");
+		
+		/***Then***/
+		assertEquals(5, voiceAnalysisList.size());
+	}
+
+}
