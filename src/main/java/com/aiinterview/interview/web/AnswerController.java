1d1beee732b6126957e08825b56d8f5dc6b17f34
ktk <ktk3981@gmail.com>
2020-12-22 12:39:33 +0900

    반복어 분석 - 스크립트 내용 없을때 조건 추가

--- src/main/java/com/aiinterview/interview/web/AnswerController.java
+++ src/main/java/com/aiinterview/interview/web/AnswerController.java
@@ -107,146 +107,158 @@ public class AnswerController {
 		}
 		
 		/* 반복어 분석 */
-		// 언어 분석 기술 문어/구어 중 한가지만 선택해 사용
-        // 언어 분석 기술(문어)
-//        String openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU";  
-        // 언어 분석 기술(구어)
-        String openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU_spoken";         
-        String accessKey = "d119ee9a-e502-4236-8f15-61ac9e8dad04";   // 발급받은 API Key
-        String analysisCode = "ner";        // 언어 분석 코드
-        String text = script;           // 분석할 텍스트 데이터
-        Gson gson = new Gson();
-         
-        Map<String, Object> request = new HashMap<>();
-        Map<String, String> argument = new HashMap<>();
- 
-        argument.put("analysis_code", analysisCode);
-        argument.put("text", text);
- 
-        request.put("access_key", accessKey);
-        request.put("argument", argument);
- 
-        URL url;
-        Integer responseCode = null;
-        String responBodyJson = null;
-        Map<String, Object> responeBody = null;
- 
-        try {
-            url = new URL(openApiURL);
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("POST");
-            con.setDoOutput(true);
- 
-            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-            wr.write(gson.toJson(request).getBytes("UTF-8"));
-            wr.flush();
-            wr.close();
- 
-            responseCode = con.getResponseCode();
-            InputStream is = con.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            StringBuffer sb = new StringBuffer();
- 
-            String inputLine = "";
-            while ((inputLine = br.readLine()) != null) {
-                sb.append(inputLine);
-            }
-            responBodyJson = sb.toString();
- 
-            // http 요청 오류 시 처리
-            if ( responseCode != 200 ) {
-                // 오류 내용 출력
-            }
- 
-            responeBody = gson.fromJson(responBodyJson, Map.class);
-            Integer result = ((Double) responeBody.get("result")).intValue();
-            Map<String, Object> returnObject;
-            List<Map> sentences;
- 
-            // 분석 요청 오류 시 처리
-            if ( result != 0 ) {
-                // 오류 내용 출력
-            }
- 
-            // 분석 결과 활용
-            returnObject = (Map<String, Object>) responeBody.get("return_object");
-            sentences = (List<Map>) returnObject.get("sentence");
- 
-            Map<String, Morpheme> morphemesMap = new HashMap<String, Morpheme>();
-            Map<String, NameEntity> nameEntitiesMap = new HashMap<String, NameEntity>();
-            List<Morpheme> morphemes = null;
-            List<NameEntity> nameEntities = null;
- 
-            for( Map<String, Object> sentence : sentences ) {
-                // 형태소 분석기 결과 수집 및 정렬
-                List<Map<String, Object>> morphologicalAnalysisResult = (List<Map<String, Object>>) sentence.get("morp");
-                for( Map<String, Object> morphemeInfo : morphologicalAnalysisResult ) {
-                    String lemma = (String) morphemeInfo.get("lemma");
-                    Morpheme morpheme = morphemesMap.get(lemma);
-                    if ( morpheme == null ) {
-                        morpheme = new Morpheme(lemma, (String) morphemeInfo.get("type"), 1);
-                        morphemesMap.put(lemma, morpheme);
-                    } else {
-                        morpheme.count = morpheme.count + 1;
-                    }
-                }
- 
-                // 개체명 분석 결과 수집 및 정렬
-                List<Map<String, Object>> nameEntityRecognitionResult = (List<Map<String, Object>>) sentence.get("NE");
-                for( Map<String, Object> nameEntityInfo : nameEntityRecognitionResult ) {
-                    String name = (String) nameEntityInfo.get("text");
-                    NameEntity nameEntity = nameEntitiesMap.get(name);
-                    if ( nameEntity == null ) {
-                        nameEntity = new NameEntity(name, (String) nameEntityInfo.get("type"), 1);
-                        nameEntitiesMap.put(name, nameEntity);
-                    } else {
-                        nameEntity.count = nameEntity.count + 1;
-                    }
-                }
-            }
- 
-            if ( 0 < morphemesMap.size() ) {
-                morphemes = new ArrayList<Morpheme>(morphemesMap.values());
-                morphemes.sort( (morpheme1, morpheme2) -> {
-                    return morpheme2.count - morpheme1.count;
-                });
-            }
- 
-            if ( 0 < nameEntitiesMap.size() ) {
-                nameEntities = new ArrayList<NameEntity>(nameEntitiesMap.values());
-                nameEntities.sort( (nameEntity1, nameEntity2) -> {
-                    return nameEntity2.count - nameEntity1.count;
-                });
-            }
-            
-            
-            // 반복어 리스트
-            List<RepeatAnalysisVO> repeatAnalysisList = new ArrayList<>();
-            
-            // 형태소들 중 명사들에 대해서 많이 노출된 순으로 출력 ( 최대 5개 )
-            morphemes
-                .stream()
-                .filter(morpheme -> {
-                    return morpheme.type.equals("NNG") ||
-                            morpheme.type.equals("NNP") ||
-                            morpheme.type.equals("NNB");
-                })
-                .limit(3)
-                .forEach(morpheme -> {
-                	// db에 보낼 반복어 객체 생성
-                	if(morpheme.count>1) {
-                		RepeatAnalysisVO repeatAnalysisVO = new RepeatAnalysisVO();
-                		repeatAnalysisVO.setRepeatContent(morpheme.text);
-                		repeatAnalysisVO.setRepeatCount(morpheme.count+"");
-                		repeatAnalysisList.add(repeatAnalysisVO);
-                	}
-                });
-            
-            analysisVO.setRepeatAnalysisList(repeatAnalysisList);
-            
-			/* 인재상 분석 */
-			List<KeywordMatchingVO> keywordMatchingList = keywordMatchingService.retrieveList();
-			
+		
+		if(answerVO.getAnsContent() != null && answerVO.getAnsContent() != "") {
+			// 언어 분석 기술 문어/구어 중 한가지만 선택해 사용
+	        // 언어 분석 기술(문어)
+//	        String openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU";  
+	        // 언어 분석 기술(구어)
+	        String openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU_spoken";         
+	        String accessKey = "d119ee9a-e502-4236-8f15-61ac9e8dad04";   // 발급받은 API Key
+	        String analysisCode = "ner";        // 언어 분석 코드
+	        String text = script;           // 분석할 텍스트 데이터
+	        Gson gson = new Gson();
+	         
+	        Map<String, Object> request = new HashMap<>();
+	        Map<String, String> argument = new HashMap<>();
+	 
+	        argument.put("analysis_code", analysisCode);
+	        argument.put("text", text);
+	 
+	        request.put("access_key", accessKey);
+	        request.put("argument", argument);
+	 
+	        URL url;
+	        Integer responseCode = null;
+	        String responBodyJson = null;
+	        Map<String, Object> responeBody = null;
+	 
+	        try {
+	            url = new URL(openApiURL);
+	            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+	            con.setRequestMethod("POST");
+	            con.setDoOutput(true);
+	 
+	            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+	            wr.write(gson.toJson(request).getBytes("UTF-8"));
+	            wr.flush();
+	            wr.close();
+	 
+	            responseCode = con.getResponseCode();
+	            InputStream is = con.getInputStream();
+	            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+	            StringBuffer sb = new StringBuffer();
+	 
+	            String inputLine = "";
+	            while ((inputLine = br.readLine()) != null) {
+	                sb.append(inputLine);
+	            }
+	            responBodyJson = sb.toString();
+	 
+	            // http 요청 오류 시 처리
+	            if ( responseCode != 200 ) {
+	                // 오류 내용 출력
+	            }
+	 
+	            responeBody = gson.fromJson(responBodyJson, Map.class);
+	            Integer result = ((Double) responeBody.get("result")).intValue();
+	            Map<String, Object> returnObject;
+	            List<Map> sentences;
+	 
+	            // 분석 요청 오류 시 처리
+	            if ( result != 0 ) {
+	                // 오류 내용 출력
+	            }
+	 
+	            // 분석 결과 활용
+	            returnObject = (Map<String, Object>) responeBody.get("return_object");
+	            sentences = (List<Map>) returnObject.get("sentence");
+	 
+	            Map<String, Morpheme> morphemesMap = new HashMap<String, Morpheme>();
+	            Map<String, NameEntity> nameEntitiesMap = new HashMap<String, NameEntity>();
+	            List<Morpheme> morphemes = null;
+	            List<NameEntity> nameEntities = null;
+	 
+	            for( Map<String, Object> sentence : sentences ) {
+	                // 형태소 분석기 결과 수집 및 정렬
+	                List<Map<String, Object>> morphologicalAnalysisResult = (List<Map<String, Object>>) sentence.get("morp");
+	                for( Map<String, Object> morphemeInfo : morphologicalAnalysisResult ) {
+	                    String lemma = (String) morphemeInfo.get("lemma");
+	                    Morpheme morpheme = morphemesMap.get(lemma);
+	                    if ( morpheme == null ) {
+	                        morpheme = new Morpheme(lemma, (String) morphemeInfo.get("type"), 1);
+	                        morphemesMap.put(lemma, morpheme);
+	                    } else {
+	                        morpheme.count = morpheme.count + 1;
+	                    }
+	                }
+	 
+	                // 개체명 분석 결과 수집 및 정렬
+	                List<Map<String, Object>> nameEntityRecognitionResult = (List<Map<String, Object>>) sentence.get("NE");
+	                for( Map<String, Object> nameEntityInfo : nameEntityRecognitionResult ) {
+	                    String name = (String) nameEntityInfo.get("text");
+	                    NameEntity nameEntity = nameEntitiesMap.get(name);
+	                    if ( nameEntity == null ) {
+	                        nameEntity = new NameEntity(name, (String) nameEntityInfo.get("type"), 1);
+	                        nameEntitiesMap.put(name, nameEntity);
+	                    } else {
+	                        nameEntity.count = nameEntity.count + 1;
+	                    }
+	                }
+	            }
+	 
+	            if ( 0 < morphemesMap.size() ) {
+	                morphemes = new ArrayList<Morpheme>(morphemesMap.values());
+	                morphemes.sort( (morpheme1, morpheme2) -> {
+	                    return morpheme2.count - morpheme1.count;
+	                });
+	            }
+	 
+	            if ( 0 < nameEntitiesMap.size() ) {
+	                nameEntities = new ArrayList<NameEntity>(nameEntitiesMap.values());
+	                nameEntities.sort( (nameEntity1, nameEntity2) -> {
+	                    return nameEntity2.count - nameEntity1.count;
+	                });
+	            }
+	            
+	            
+	            // 반복어 리스트
+	            List<RepeatAnalysisVO> repeatAnalysisList = new ArrayList<>();
+	            
+	            // 형태소들 중 명사들에 대해서 많이 노출된 순으로 출력 ( 최대 5개 )
+	            morphemes
+	                .stream()
+	                .filter(morpheme -> {
+	                    return morpheme.type.equals("NNG") ||
+	                            morpheme.type.equals("NNP") ||
+	                            morpheme.type.equals("NNB");
+	                })
+	                .limit(3)
+	                .forEach(morpheme -> {
+	                	// db에 보낼 반복어 객체 생성
+	                	if(morpheme.count>1) {
+	                		RepeatAnalysisVO repeatAnalysisVO = new RepeatAnalysisVO();
+	                		repeatAnalysisVO.setRepeatContent(morpheme.text);
+	                		repeatAnalysisVO.setRepeatCount(morpheme.count+"");
+	                		repeatAnalysisList.add(repeatAnalysisVO);
+	                	}
+	                });
+	            
+	            analysisVO.setRepeatAnalysisList(repeatAnalysisList);
+	        } catch (MalformedURLException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }catch (Exception e) {
+				e.printStackTrace();
+			}  
+		}
+		
+        
+		/* 인재상 분석 */
+		List<KeywordMatchingVO> keywordMatchingList;
+		try {
+			keywordMatchingList = keywordMatchingService.retrieveList();
 			List<KeywordAnalysisVO> keywordAnalysisList = new ArrayList<>();
 			
 			for(int i=0; i<keywordMatchingList.size(); i++) {
@@ -261,22 +273,23 @@ public class AnswerController {
 			}
 			
 			analysisVO.setKeywordAnalysisList(keywordAnalysisList);
-			
-			
-			/* 음성 분석*/
-			List<VoiceAnalysisVO> voiceAnalysisList = voiceAnalysisVO.getVoiceAnalysisVOLIst();
-			
-			analysisVO.setVoiceAnalysisList(voiceAnalysisList);
-					
-			answerSeivce.create(analysisVO);
-				
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+			
+			
+			
+		/* 음성 분석*/
+		List<VoiceAnalysisVO> voiceAnalysisList = voiceAnalysisVO.getVoiceAnalysisVOLIst();
+		
+		analysisVO.setVoiceAnalysisList(voiceAnalysisList);
+				
+		try {
+			answerSeivce.create(analysisVO);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+				
 		
 		return "jsonView";
 	}
