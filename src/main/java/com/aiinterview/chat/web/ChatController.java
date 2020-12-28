8045101017446ca12b114ab2563405c22675f10d
lss <paresf4@gmail.com>
2020-12-28 13:34:35 +0900

    import 오류 수정

--- src/main/java/com/aiinterview/chat/web/ChatController.java
+++ src/main/java/com/aiinterview/chat/web/ChatController.java
@@ -6,10 +6,8 @@ import java.util.List;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
-import org.eclipse.jdt.internal.compiler.ast.ModuleDeclaration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
